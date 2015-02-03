a55b0b5aa8a267be2ccf88e17fb0a04e3db53ab8
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-02-03 22:15:26 +0100

    implement first draft of tests for process function

--- test/cli_process_test.exs
+++ test/cli_process_test.exs
@@ -27,36 +27,113 @@ defmodule CliProcessTest do
     """
   end
 
-  test "add" do
+  test "add new link" do
+    result = capture_io fn -> process({:add, ["http://example.com"],
+                                      [{:tags, "tag"}, 
+                                       {:description, "description"}]}) end
+    assert result == """
+    Link added
+    ==========
+    Link               | Tags | Description
+    -------------------+------+------------
+    http://example.com | tag  | description 
+    """
   end
 
   test "update all values" do
-  end
-
-  test "update the tags" do
-  end
-
-  test "update the description" do
+    result = capture_io fn -> process({:update, ["http://example.com"],
+                                      [{:tags, "tag 1"}, 
+                                       {:description, "description 1"}]}) end
+    assert result == """
+    Link updated
+    ============
+    Link               | Tags  | Description
+    -------------------+-------+--------------
+    http://example.com | tag 1 | description 1
+    """
   end
 
   test "remove" do
+    result = capture_io fn -> process({:remove, ["http://example.com"], []}) end
+
+    assert result == """
+    Link removed
+    ============
+    """
   end
 
   test "check a specific URI" do
-  end
+    result = capture_io fn -> process({:check, ["http://example.com",
+                                               "http://example.cmo"], []}) end
 
-  test "check all URIs" do
+    assert result == """
+    Check results
+    =============
+    Link               | Response
+    -------------------+---------
+    http://example.com | ok
+    http://example.cmo | 404
+    """
   end
 
   test "check URIs based on a filter" do
+    process({:add, ["http://example.com"], 
+                  [{:tag, "tag"}, {:description, "description"}]})
+
+    result = capture_io fn -> process({:check, [], 
+                                      [{:tag, "tag"}, 
+                                       {:description, "description"}]}) end
+    assert result == """
+    Check results
+    =============
+    Link               | Response
+    -------------------+---------
+    http://example.com | ok
+    """
   end
 
   test "list a specific URI" do
+    process({:add, ["http://example.com"], 
+                  [{:tag, "tag"}, {:description, "description"}]})
+
+    result = capture_io fn -> process({:list, ["http://example.com"], []}) end
+
+    assert result == """
+    Link updated
+    ============
+    Link               | Tags | Description
+    -------------------+------+------------
+    http://example.com | tag  | description
+    """
   end
 
   test "list all URIs" do
+    process({:add, ["http://example.com"], 
+                  [{:tag, "tag"}, {:description, "description"}]})
+
+    result = capture_io fn -> process({:list, [], []}) end
+
+    assert result == """
+    Link updated
+    ============
+    Link               | Tags | Description
+    -------------------+------+------------
+    http://example.com | tag  | description
+    """
   end
 
   test "list URIs based on a filter" do
+    process({:add, ["http://example.com"], 
+                  [{:tag, "tag"}, {:description, "description"}]})
+
+    result = capture_io fn -> process({:list, [], [{:tag, "tag"}]}) end
+
+    assert result == """
+    Link updated
+    ============
+    Link               | Tags | Description
+    -------------------+------+------------
+    http://example.com | tag  | description
+    """
   end
 end
